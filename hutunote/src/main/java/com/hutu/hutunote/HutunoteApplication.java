6fb897e9fd1205da835a0ca02240516954696044
liaojb <1161086097@qq.com>
2022-08-17 10:30:28 +0800

    初始化

---
+++ hutunote/src/main/java/com/hutu/hutunote/HutunoteApplication.java
@@ -0,0 +1,13 @@
+package com.hutu.hutunote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HutunoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HutunoteApplication.class, args);
+    }
+
+}
---
+++ hutunote/src/test/java/com/hutu/hutunote/HutunoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hutu.hutunote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HutunoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
