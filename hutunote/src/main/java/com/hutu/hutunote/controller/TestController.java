f6a8ea82efc620c8e857434d959146ec5fb1373a
HutuBing <1161086097@qq.com>
2025-09-13 13:31:17 +0800

    kong3

--- hutunote/src/main/java/com/hutu/hutunote/controller/TestController.java
+++ hutunote/src/main/java/com/hutu/hutunote/controller/TestController.java
@@ -1,14 +1,16 @@
 package com.hutu.hutunote.controller;
 
 import com.hutu.hutunote.common.Result;
+import com.hutu.hutunote.execute.BookExecute;
 import com.hutu.hutunote.execute.KongfuziExecute;
 import com.hutu.hutunote.model.vo.KongIsbnInfoVo;
 import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @Api(tags = "DEMO接口")
@@ -18,6 +20,8 @@ public class TestController extends BaseController {
 
     @Autowired
     private KongfuziExecute kongfuziExecute;
+    @Autowired
+    private BookExecute bookExecute;
 
     @GetMapping("/test")
     public Result<List<KongIsbnInfoVo>> test() {
@@ -27,6 +31,22 @@ public class TestController extends BaseController {
     @GetMapping("/test2")
     public Result<List<KongIsbnInfoVo>> test2() {
         return Result.OK(kongfuziExecute.getIsbnInfoList());
+    }
+
+    @PostMapping("/uploadBook")
+    public void uploadBook(MultipartFile file) {
+        bookExecute.uploadBook(file);
+    }
+
+    @GetMapping("/downloadBook/{inventory}")
+    public void downloadBook(@PathVariable("inventory") Long inventory, HttpServletResponse response) throws Exception{
+        bookExecute.downloadBook(inventory, response);
+    }
+
+    @GetMapping("/refreshBook")
+    @PostConstruct
+    public void refreshBook() {
+        bookExecute.refreshBook();
     }
 
 }
